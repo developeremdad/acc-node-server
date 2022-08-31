39c9ee28c44ef2a0f1150882ebbd7e08b1cf069d
developeremdad <developeremdad@gmail.com>
2022-08-31 19:23:04 +0600

    stream buffer implement

---
+++ streamBuffer.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const readStream = fs.createReadStream('./streamText.txt')
+readStream.on('data', (chunk) =>{
+    console.log(chunk);
+})
+
+setTimeout(()=>{
+    readStream.pause();
+    console.log('Stream is pause');
+}, 25)
+
+setTimeout(() => {
+    readStream.resume()
+    console.log('Resume stream.');
+}, 5000);
